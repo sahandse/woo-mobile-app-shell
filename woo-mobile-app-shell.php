4a58b49c7d0280836b915021ea8e6c6e5fa77632
Sahand Rezvan <sahand.rezvanazar@gmail.com>
2026-09-19 18:12:03 +0330

    fix: flush PWA rewrite routes on activation and deactivation

--- woo-mobile-app-shell.php
+++ woo-mobile-app-shell.php
@@ -375,3 +375,16 @@ final class WMAS_Plugin {
 }
 
 new WMAS_Plugin();
+
+register_activation_hook(__FILE__, function(){
+    add_rewrite_rule('^wmas-manifest\\.webmanifest
+,'index.php?wmas_pwa=manifest','top');
+    add_rewrite_rule('^wmas-sw\\.js
+,'index.php?wmas_pwa=sw','top');
+    add_rewrite_tag('%wmas_pwa%','([^&]+)');
+    flush_rewrite_rules(false);
+});
+
+register_deactivation_hook(__FILE__, function(){
+    flush_rewrite_rules(false);
+});
